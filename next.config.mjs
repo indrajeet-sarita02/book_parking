44896925aa808b59525fbf876674e1562a216345
Indrajeet Singh <indrajeet.sarita02@gmail.com>
2026-06-30 14:32:46 +0530

    update data code

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['sequelize', 'pg'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('pg', 'sequelize');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
